320a5e4a2ee02acfe8fde4e90b6a3695bd395b6f
Fernando Jinzenji <fernandoj@me.com>
2017-08-03 21:05:34 -0700

    added git ignore and initial firebase configuration

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -7,19 +7,26 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Auth.auth().signIn(withEmail: "fernandoj@me.com", password: "nananda78") { (opUser, opError) in
+            
+            if opError != nil {
+                print ("auth error")
+            }
+            else {
+                print ("auth ok")
+                let ref = Database.database().reference(withPath: "childnode")
+                ref.setValue("test")
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
